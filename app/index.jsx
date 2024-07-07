ee3842bb6f79559f5477f62d80be4874339bc2eb
Dannysyed <dannysyed20@gmail.com>
2024-07-07 07:23:50 -0400

    fonts loaded

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Doras</Text>
+      <Text className="text-3xl font-pblack">Dora</Text>
       <StatusBar style="auto" />
       <Link href={"/Profile"} style={{ color: "blue" }}>
         Go to the Profiles
